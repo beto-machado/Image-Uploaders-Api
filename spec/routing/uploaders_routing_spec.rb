ddfab84def02f62b9d4dc2f6d992301db41c4041
beto-machado <luisrobertobeto3@gmail.com>
2022-02-22 09:20:40 -0300

    added test and docs postman

--- spec/routing/uploaders_routing_spec.rb
+++ spec/routing/uploaders_routing_spec.rb
@@ -3,27 +3,27 @@ require "rails_helper"
 RSpec.describe UploadersController, type: :routing do
   describe "routing" do
     it "routes to #index" do
-      expect(get: "/uploaders").to route_to("uploaders#index")
+      expect(get: "/api/uploaders").to route_to("uploaders#index")
     end
 
     it "routes to #show" do
-      expect(get: "/uploaders/1").to route_to("uploaders#show", id: "1")
+      expect(get: "/api/uploaders/1").to route_to("uploaders#show", id: "1")
     end
 
     it "routes to #create" do
-      expect(post: "/uploaders").to route_to("uploaders#create")
+      expect(post: "/api/uploaders").to route_to("uploaders#create")
     end
 
     it "routes to #update via PUT" do
-      expect(put: "/uploaders/1").to route_to("uploaders#update", id: "1")
+      expect(put: "/api/uploaders/1").to route_to("uploaders#update", id: "1")
     end
 
     it "routes to #update via PATCH" do
-      expect(patch: "/uploaders/1").to route_to("uploaders#update", id: "1")
+      expect(patch: "/api/uploaders/1").to route_to("uploaders#update", id: "1")
     end
 
     it "routes to #destroy" do
-      expect(delete: "/uploaders/1").to route_to("uploaders#destroy", id: "1")
+      expect(delete: "/api/uploaders/1").to route_to("uploaders#destroy", id: "1")
     end
   end
 end
